4b48f893ab8fecf7792587b202e5d53d7b9f3421
Arsan Irianto <arsan.irianto@gmail.com>
2019-02-26 17:06:06 +0800

    add Team.players resolvers

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga')
 const fetch = require('node-fetch')
-
-const restApi = `https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4328`
+const urlLookupLeague = `https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4328`
+const urlPlayers = `https://www.thesportsdb.com/api/v1/json/1/lookup_all_players.php?id=`
 
 const typeDefs = `
-  type Query {
-    teams: [Team!]!
-  }
-  type Team{
-    idTeam: String
-    strTeam: String
-    intFormedYear: String
-  }
+type Query {
+  teams: [Team!]!
+  players(id: ID!): [Player!]!
+}
+type Team {
+  idTeam: ID!
+  strTeam: String!
+  players: [Player]
+}
+type Player {
+  idTeam: ID!
+  strPlayer: String!
+}
 `
-function getTeams() {
-  return fetch(`${restApi}`).then(res => res.json()).then(res => res.teams)
-}
-
 const resolvers = {
-  Query:{
-    teams: () => getTeams()
+  Query: {
+    teams: () => {
+      return fetch(`${urlLookupLeague}`).then(res => res.json()).then(res => res.teams)
+    }
+  },
+  Team: {
+    players: parent =>  {
+      return fetch(`${urlPlayers}${parent.idTeam}`).then(res => res.json()).then(res => res.player)
+    }
   }
 }
-
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
 })
-
 server.start(() => console.log(`Server Running on http://localhost:4000`))
-
-
-
